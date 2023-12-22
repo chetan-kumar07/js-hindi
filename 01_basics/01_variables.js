d641c8df493f1a41be46900decd9e3004ad377fb
Chetan Kumar <93515190+chetan-kumar07@users.noreply.github.com>
2023-12-22 08:14:18 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+  const accountId=144553    // change nhi kiya ja sakta 
+  let accountEmail="chetan@google.com"
+  var accountPassword="12345"
+  accountCity="Jaipur"
+  let accountState;
+  //accountId=2     // Not allowed
+ 
+
+
+  accountEmail="myEmail@email.com"
+  accountPassword="347898765"
+  accountCity="farrukhabad"
+  console.log(accountId);   
+
+  /* 
+  Prefer not to use var 
+  because of issue in block scope & functional scope.
+  */
+  console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // We are using nodejs not browser
+
+/*datatypes
+
+number => 2 to power 53
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+*/
+let age =90
+console.log(typeof "Ram");
+console.log(typeof age);
+
+console.log(typeof null)   // object
+console.log(typeof undefined)   // undefined
