98352fa0579a6b405737f64a09cc223f50d59bb9
Chetan Kumar <93515190+chetan-kumar07@users.noreply.github.com>
2023-12-29 06:42:34 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score ="chetan" 
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "44" --> 44
+// "34bfb" --> NaN (Not a Number)
+// true --> 1 , false --> 0
+
+console.log("--------------------------------------------------")
+
+let isLoggedIn=""
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+// 1 --> true , 0 --> false 
+// "" --> false , "chetan" --> true
+
+console.log("--------------------------------------------------")
+
+let someNumber =87
+let StringNumber=String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
