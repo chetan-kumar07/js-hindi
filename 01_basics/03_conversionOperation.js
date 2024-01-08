d8dc00f9c8ac947c07a9b471f34b7c879559495b
Chetan Kumar <93515190+chetan-kumar07@users.noreply.github.com>
2024-01-08 06:39:59 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,51 @@
-let score ="chetan" 
-console.log(typeof score);
-console.log(typeof(score));
+// let score ="chetan" 
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// "44" --> 44
-// "34bfb" --> NaN (Not a Number)
-// true --> 1 , false --> 0
+// // "44" --> 44
+// // "34bfb" --> NaN (Not a Number)
+// // true --> 1 , false --> 0
 
-console.log("--------------------------------------------------")
+// console.log("--------------------------------------------------")
 
-let isLoggedIn=""
-let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
-// 1 --> true , 0 --> false 
-// "" --> false , "chetan" --> true
+// let isLoggedIn=""
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+// // 1 --> true , 0 --> false 
+// // "" --> false , "chetan" --> true
 
-console.log("--------------------------------------------------")
+// console.log("--------------------------------------------------")
 
-let someNumber =87
-let StringNumber=String(someNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+// let someNumber =87
+// let StringNumber=String(someNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber)
+
+// ***************** Operations ******************//
+
+let value =10
+let negValue =-value
+console.log(negValue)
+
+let str1="Hello"
+let str2 =" Chetan"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+
+console.log("1"+2+2)
+
+console.log(+true)
+console.log(+"")
+
+let num1,num2,num3=2+2
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
